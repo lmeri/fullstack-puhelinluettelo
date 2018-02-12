5c8d47633f1bfd5181ffe654d3a5e605b9cab553
Lotta Merisaari <merisaari.lotta@gmail.com>
2018-02-12 18:50:44 +0200

    small order fix

--- mongo.js
+++ mongo.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose')
-const url = process.env.MONGODB_URI
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
+
+const url = process.env.MONGODB_URI
 
 mongoose.connect(url)
 
