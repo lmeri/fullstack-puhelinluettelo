e463ab44c55a5abdf6b7bb1febaa33e1d7651553
Lotta Merisaari <merisaari.lotta@gmail.com>
2018-02-12 14:49:56 +0200

    initiate app, heroku

--- index.js
+++ index.js
@@ -94,7 +94,8 @@ function checkPersons(name) {
 }
   
   
-const PORT = 3001
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
 })
+
